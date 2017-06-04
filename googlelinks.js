105b28266b5b0b83b296819226c64d6d935217ae
brayan <brayan.oduro@gmail.com>
2017-06-04 14:38:47 +0200

    teste

--- googlelinks.js
+++ googlelinks.js
@@ -2,33 +2,25 @@ var links = [];
 var casper = require('casper').create();
 
 function getLinks() {
-    var links = document.querySelectorAll('h3.r a');
+    var links = document.querySelectorAll('section.tabsContent ul li a');
     return Array.prototype.map.call(links, function(e) {
         return e.getAttribute('href');
     });
 }
 
-casper.start('http://google.fr/', function() {
+casper.start('https://www.leboncoin.fr/telephonie/offres/ile_de_france/?th=1&parrot=0', function() {
    // Wait for the page to be loaded
-   this.waitForSelector('form[action="/search"]');
+   this.waitForSelector('list');
 });
 
-casper.then(function() {
-   // search for 'casperjs' from google form
-   this.fill('form[action="/search"]', { q: 'casperjs' }, true);
-});
+
 
 casper.then(function() {
     // aggregate results for the 'casperjs' search
     links = this.evaluate(getLinks);
-    // now search for 'phantomjs' by filling the form again
-    this.fill('form[action="/search"]', { q: 'phantomjs' }, true);
 });
 
-casper.then(function() {
-    // aggregate results for the 'phantomjs' search
-    links = links.concat(this.evaluate(getLinks));
-});
+
 
 casper.run(function() {
     // echo results in some pretty fashion
